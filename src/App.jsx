10547d16c763b96614af5bbc635aaaa45239f3d1
Steven Busenbarrick <sbusenba@gmail.com>
2024-07-15 16:35:01 -0700

    create TopNav element

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import TopNav from "./components/TopNav";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
+      <TopNav></TopNav>
       <h1 className="bondyes">
         Bond <span className="green">YES</span>!
       </h1>
---
+++ src/components/TopNav.jsx
@@ -0,0 +1,17 @@
+import "./TopNav.css";
+
+function TopNav () {
+
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Voice Your Support</li>
+        <li>The Opposition</li>
+        <li>About Me</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default TopNav;
