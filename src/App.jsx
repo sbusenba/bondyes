1e7ddb8f0ed5527b7a180e4dbb74a6eaaa8dcccd
Steven Busenbarrick <sbusenba@gmail.com>
2024-07-17 11:12:08 -0700

    add router and paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import TopNav from "./components/TopNav";
+import Home from "./pages/Home";
+import Opposition from "./pages/Opposition";
+import AboutMe from "./pages/AboutMe";
+import Support from "./pages/Support";
+
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/support",
+      element: <Support />,
+    },
+    {
+      path: "/opposition",
+      element: <Opposition />,
+    },
+    {
+      path: "/about",
+      element: <AboutMe />,
+    },
+  ]);
   return (
     <>
       <TopNav></TopNav>
@@ -11,6 +35,7 @@ function App() {
       <div className="card">
         <p>Under construction, check back soon!</p>
       </div>
+      <RouterProvider router={router} />
     </>
   );
 }
