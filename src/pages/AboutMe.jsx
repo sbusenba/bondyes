25c15ab64fdbab458bc68c6eb2ee674a3c7b53af
Steven Busenbarrick <sbusenba@gmail.com>
2024-09-04 12:34:49 -0700

    add content to aboute me page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,8 +2,27 @@ import "/src/pages/aboutme.css";
 const AboutMe = () => {
   return (
     <div>
-      <h1>About Me</h1>
-      <img src="headshot.jpg"></img>
+      <div className="about-me">
+        <div className="headshot">
+          <img src="public/imgs/headshot.jpg"></img>
+        </div>
+        <h3>Steven Busenbarrick</h3>
+        <h4>About me</h4>
+        <p>
+          I am an aspiring web developer with a bachelors degree in biomedical
+          sciences. For the past year and a half I have been following the Odin
+          Project curriculum. I&apos;ve learned some pretty cool things and this
+          site&apos;s purpose is to share them with you.
+        </p>
+        <div className="about-links">
+          <a href="https://github.com/sbusenba">
+            <img src="public/imgs/github-mark.svg" />
+          </a>
+          <a href="https://www.linkedin.com/in/steven-busenbarrick-71058a80/">
+            <img src="public/imgs/linkedin-svgrepo-com.svg" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
