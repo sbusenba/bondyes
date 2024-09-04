84b2951e06bbf442c0b00e98ed82fdc514972e7a
Steven Busenbarrick <sbusenba@gmail.com>
2024-09-04 13:25:23 -0700

    update bio, center photo on mobile

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -9,10 +9,10 @@ const AboutMe = () => {
         <h3>Steven Busenbarrick</h3>
         <h4>About me</h4>
         <p>
-          I am an aspiring web developer with a bachelors degree in biomedical
-          sciences. For the past year and a half I have been following the Odin
-          Project curriculum. I&apos;ve learned some pretty cool things and this
-          site&apos;s purpose is to share them with you.
+          I am an aspiring web developer with a bachelors degree in Biomedical
+          Sciences from the University of South Florida. For the past year and a
+          half I have been following the Odin Project curriculum, teaching
+          myself web development, and working on my own passion projects.
         </p>
         <div className="about-links">
           <a href="https://github.com/sbusenba">
