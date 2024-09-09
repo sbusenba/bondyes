f24ce8f2bd9df3842b25bab816d6fa8b9d1a1996
Steven Busenbarrick <sbusenba@gmail.com>
2024-09-09 14:04:09 -0700

    refactor aboutme css

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,24 +2,31 @@ import "/src/pages/aboutme.css";
 const AboutMe = () => {
   return (
     <div>
+      <h3>Steven Busenbarrick</h3>
+      <hr />
+      <h4>About me</h4>
       <div className="about-me">
-        <div className="headshot">
-          <img src="public/imgs/headshot.jpg"></img>
+        <div className="bio">
+          <div className="headshot">
+            <img src="imgs/headshot.jpg"></img>
+          </div>
+
+          <p>
+            I am an aspiring web developer with a bachelors degree in Biomedical
+            Sciences from the University of South Florida. For the past year and
+            a half I have been following the Odin Project curriculum, teaching
+            myself web development, and working on my own passion projects. I
+            have a son in Ridgefield School District, and believe funding
+            education will not only help him, but all children in the district,
+            and the community as a whole.
+          </p>
         </div>
-        <h3>Steven Busenbarrick</h3>
-        <h4>About me</h4>
-        <p>
-          I am an aspiring web developer with a bachelors degree in Biomedical
-          Sciences from the University of South Florida. For the past year and a
-          half I have been following the Odin Project curriculum, teaching
-          myself web development, and working on my own passion projects.
-        </p>
         <div className="about-links">
           <a href="https://github.com/sbusenba">
-            <img src="public/imgs/github-mark.svg" />
+            <img src="imgs/github-mark.svg" />
           </a>
           <a href="https://www.linkedin.com/in/steven-busenbarrick-71058a80/">
-            <img src="public/imgs/linkedin-svgrepo-com.svg" />
+            <img src="imgs/linkedin-svgrepo-com.svg" />
           </a>
         </div>
       </div>
