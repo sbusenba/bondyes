13b76b31c61983a6a7682def6fd120be55fec548
Steven Busenbarrick <sbusenba@gmail.com>
2024-09-25 18:39:43 -0700

    top nav is dropdown for small screens

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,24 +1,47 @@
 import "./TopNav.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function TopNav() {
+  let [expanded, setExpanded] = useState(false);
+  let update = (e) => {
+    setExpanded(!expanded);
+    console.log(e);
+  };
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/">
-            <span className="black">Bond</span>{" "}
-            <span className="green">Yes!</span>
+        <li className={expanded ? "nav-item expanded" : "nav-item"}>
+          <Link to="/" onClick={(e) => update(e)}>
+            <button>
+              <h2 className="bondyes">
+                {"Bond "} <span className="green">YES!</span>
+              </h2>
+            </button>
           </Link>
         </li>
-        <li>
-          <Link to="/support">Voice Your Support</Link>
+
+        <li className={expanded ? "nav-item expanded" : "nav-item"}>
+          <Link to="/support" onClick={(e) => update(e)}>
+            <button>Support</button>
+          </Link>
         </li>
-        <li>
-          <Link to="/opposition">The Opposition</Link>
+        <li className={expanded ? "nav-item expanded" : "nav-item"}>
+          <Link to="/opposition" onClick={(e) => update(e)}>
+            <button>Opposition</button>
+          </Link>
         </li>
-        <li>
-          <Link to="/aboutme">About Me</Link>
+
+        <li className={expanded ? "nav-item expanded" : "nav-item"}>
+          <Link to="/aboutme" onClick={(e) => update(e)}>
+            <button onClick={(e) => update(e)}>About Me</button>
+          </Link>
+        </li>
+
+        <li className="nav-expand-button">
+          <button onClick={(e) => update(e)}>
+            {expanded ? "Collapse" : "Menu"}
+          </button>
         </li>
       </ul>
     </nav>
